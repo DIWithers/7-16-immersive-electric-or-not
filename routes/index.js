04dcc1a062d66cca19fa08ef36000dba29d3a580
DIWithers <d.ivy.withers@gmail.com>
2016-09-08 16:37:00 -0400

    init commit of node.js voting app

--- routes/index.js
+++ routes/index.js
@@ -52,14 +52,39 @@ mongoClient.connect(mongoUrl, function(error, database) {
 // Nodemon
 
 router.get('/', function(req, res, next) {
-//2. Get pics from Mongo and store them in an array to pass to view
-	db.collection("images").find().toArray(function(error, photos) {
-		//3. Grab a random image from that array		
-		// console.log(photos); //logs to terminal
-		var randomNum = Math.floor(Math.random() * photos.length);
-		var randomPhoto = photos[randomNum].imgSrc;
-		//4. Send that image to the view
-		res.render('index', { imageToRender: randomPhoto });
+//5. Find all the photos the user has voted on and load an array up with them
+	var userIP = req.ip;
+	db.collection("votes").find({ip: userIP}).toArray(function(error, userResult) {
+		var photosVoted = [];
+		if (error) {
+			console.log("There was an error fetching user votes");
+		}
+		else {
+			
+			// console.log(userResult);
+			for (var i = 0; i < userResult.length; i++) {
+				photosVoted.push(userResult[i].image);
+				console.log(photosVoted);
+			}
+		}
+	
+
+		//2. Get pics from Mongo and store them in an array to pass to view
+		//6. Limit the query to photos not voted on //nin = not in
+		db.collection("images").find({imgSrc: {$nin: photosVoted} }).toArray(function(error, photos) {
+			if (photos.length === 0) {
+				// res.send("You have voted on all images");
+				res.redirect("/standings");
+			}
+			else {
+			//3. Grab a random image from that array		
+			// console.log(photos); //logs to terminal
+			var randomNum = Math.floor(Math.random() * photos.length);
+			var randomPhoto = photos[randomNum].imgSrc;
+			//4. Send that image to the view
+			res.render('index', { imageToRender: randomPhoto });
+			}
+		});
 	});
 });
 
@@ -68,13 +93,61 @@ router.post("/electric", function(req, res, next) {
 	//1. We know whether they voted electric or poser because it's in req.body
 	//2. We know what image they voted on bc it's in req.body.image
 	//3. We know who they are bc we have their IP address
+	if (req.body.submit === "Electric!") {
+		var upDownVote = 1;
+		// res.json("You clicked electric");
+	}
+	else if (req.body.submit === "Poser!") {
+		// res.json("You clicked poser");
+		var upDownVote = -1;
+	}
 	db.collection("votes").insertOne({
 		ip: req.ip,
 		vote: req.body.submit,
 		image: req.body.image
 	});
+	//7. Update images collection so the image voted on will have # of votes
+	db.collection("images").find({imgSrc: req.body.image}).toArray(function(error, result) {
+		if (isNaN(result[0].totalVotes)) {
+			total = 0;
+		}
+		else {
+			total = result[0].totalVotes;
+		}
+		db.collection("images").updateOne(
+			{imgSrc: req.body.image},
+			{
+				$set: {"totalVotes": (total + upDownVote)}
+			}, function(error, results) {
+				//Check to see if there is an error
+				//Check to see if the document was updated
+			}
+		)
+	})
+	
 	res.redirect("/");
 });
 
+router.get("/standings", function(req, res, next) {
+	db.collection("images").find().toArray(function(error, allResults) {
+		// var standingsArray = [];
+		allResults.sort(function(a,b) { //quick or merge sort in Chrome?
+			// console.log();
+			return (b.totalVotes - a.totalVotes);
+		});
+		res.render("standings", {theStandings: allResults});
+		
+	});
+});
+router.get("/resetUserVotes", (req, res, next) => {
+	db.collection("votes").deleteMany(
+		{ip: req.ip},
+		function(error, results) {
+
+		}
+	);
+	res.redirect("/");
+	
+});
 
 module.exports = router;
